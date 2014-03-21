bad2a0e9692c83e3803d3f7fc45d310546be812d
Adam Davis <davisadam10@googlemail.com>
2014-03-21 21:42:15 +0000

    Added the first set of utils

---
+++ utils.py
@@ -0,0 +1,35 @@
+import maya.cmds as cmds
+nodes = cmds.ls(sl=True)
+
+class AttributeExport(object):
+    def __init__(self):
+        self.name = None
+        self.type = None
+        self.value = None
+
+class NodeExport(object):
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.attrs = []
+
+    def addAttr(self, name, type, value):
+        attr = AttributeExport()
+        attr.name = name
+        attr.type = type
+        attr.value = value
+        self.attrs.append(attr)
+
+allNodes = []
+for node in nodes:
+    exportNode = NodeExport(node, cmds.nodeType(node))
+    attrs = cmds.listAttr(node, ud=True)
+    for attr in attrs:
+        attrType = cmds.getAttr("%s.%s" % (node, attr), type=True)
+        value = cmds.getAttr("%s.%s" % (node, attr))
+        print attr, attrType, value
+        exportNode.addAttr(attr, attrType, value)
+    allNodes.append(exportNode)
+    
+allNodes[0].attrs[0].value
+    
